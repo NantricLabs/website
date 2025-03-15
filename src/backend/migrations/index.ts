bec7df619ad84d2b81e2a34f8ff7ef9b2ebc49e6
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2025-03-16 00:04:00 +0100

    Migrations

--- src/backend/migrations/index.ts
+++ src/backend/migrations/index.ts
@@ -1,2 +1,9 @@
+import * as migration_20250315_230352 from './20250315_230352';
 
-export const migrations = []
+export const migrations = [
+  {
+    up: migration_20250315_230352.up,
+    down: migration_20250315_230352.down,
+    name: '20250315_230352'
+  },
+];
